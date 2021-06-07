6c2f529dd35ca64e44469a7a7d8718b4f7ac695a
Suraj Rai <77954411+devsurajrai@users.noreply.github.com>
2021-06-07 20:18:48 +0530

    docs: added readme.md, style: indentation

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Hello Everyone
+
+This CLI App is a basic implimemtaion of programming using Javascript. It has been made while the live session Lesson 01,Level Zero by Tanay Sir. This is just an introduction to the programming. It consosts of the questions about myself which my friends would have to answer and on the basis of points scored it will be judged how well he/she know me.
+
+To play the game kindly visit [Click to play](https://replit.com/@SurajRai4/doYouKnowMe?embed=1&output=1#index.js)
+
+### Below are the questions with answers for testing purposes:
+
+
+        1. question: "What is my nick name?",
+           answer: "Churan"
+    
+
+        2. question: "What is my age?",
+           answer: "23"
+    
+
+        3. question: "Do I like rafting?",
+           answer: "Yes"
+
+
+        4. question: "What is my favourite street food?",
+           answer: "Chilli Potato"
+
+
+        5. question: "Which is my favourite chocolate?",
+           answer: "Dark Chocolate"
+    
+
---
+++ index.js
@@ -0,0 +1,68 @@
+var readlineSync = require('readline-sync')
+
+var userName = readlineSync.question("May I Know Your Good Name Please? ");
+
+console.log('Welcome ' + userName + " Let's see how well you know \'SURAJ RAI\'");
+
+console.log("Here We Go !");
+console.log(" ");
+
+
+var score = 0;
+var correctAnswer = 0;
+var usernameAndScore = [{
+    playerName: "ShubhamPal",
+    points: "4"
+}]
+
+var questionAnswers = [
+
+    {
+        question: "What is my nick name?",
+        answer: "Churan"
+    },
+
+    {
+        question: "What is my age?",
+        answer: "23"
+    },
+
+    {
+        question: "Do I like rafting?",
+        answer: "Yes"
+    },
+
+    {
+        question: "What is my favourite street food?",
+        answer: "Chilli Potato"
+    },
+
+    {
+        question: "Which is my favourite chocolate?",
+        answer: "Dark Chocolate"
+    }
+
+]
+
+for (var i = 0; i < questionAnswers.length; i++) {
+
+    console.log(questionAnswers[i].question);
+    var userAnswer = readlineSync.question("Answer: ");
+
+    if (questionAnswers[i].answer.toUpperCase() === userAnswer.toUpperCase()) {
+        console.log("You are right!");
+        score++;
+        correctAnswer++;
+        console.log(" ");
+    } else {
+        console.log("You are Wrong");
+        score--;
+        console.log(" ");
+    }
+}
+
+console.log("Thank You ! You gave " + correctAnswer + " Out Of " + questionAnswers.length + " Right Answers " + ". Your Score is " + score)
+
+console.log("The high score is " + usernameAndScore[0].points + " Scored by " + usernameAndScore[0].playerName)
+
+console.log("Please send the screenshot if you have beaten the high score !")
